6a84dc3f958b61d26ef013641f59b700c80078f2
CodePwn2021 <77036672+CodePwn2021@users.noreply.github.com>
2024-01-02 17:10:41 +0800

    0.1.0

--- readme.md
+++ readme.md
@@ -2,15 +2,18 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-lolimi-ai-chat?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-lolimi-ai-chat)
 
-Koishi插件，调用桑帛云API实现AI聊天bot，有两个角色：沫沫（软萌妹妹）和婧枫（傲娇姐姐）。
+Koishi插件，调用桑帛云API实现AI聊天bot（该AI没有记忆），有两个角色（沫沫-软萌妹妹、婧枫-傲娇姐姐），并且可自定义角色提示词。
 
 貌似是百度的文心一言模型，引用一下部分聊天记录：
 
 > 我：你是openai开fa的吗
+>
 > bot：我是通过百度研发的文心大模型技术，运行在百度的飞桨深度学习平台上的，依托于飞桨深度学习平台和文心知识增强大模型，持续从海量数据和大规模知识中融合学习，具备知识增强、检索增强和对话增强的技术特色。
 
 而且API居然会屏蔽“开发的”这个关键词（应该还有别的，懒得一个个试）
 
 额外的信息：
+
 ~~水commit用，新的一年多水几条commit和仓库~~
+
 为什么插件要叫lolimi-chat-ai呢，是因为[桑帛云API](https://api.lolimi.cn)的域名是 api.lolimi.cn
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'lolimi-ai-chat';
 
-export const usage = '接口来自桑帛云 (https://api.lolimi.cn)\n权限说明：获取用户的权限等级需要用到database服务';
+export const usage = '接口来自桑帛云 (https://api.lolimi.cn) 权限说明：获取用户的权限等级需要用到database服务';
 
 export const inject = ['database'];
 
 export interface Config {
   chatRole: ChatRole,
+  chatRolePrompt?: string,
   usePrefixTriggerInChannel: boolean,
   prefix: string,
   authority: KoishiAuthority
@@ -15,7 +16,8 @@ export interface Config {
 
 enum ChatRole {
   JinFeng,
-  MoMo
+  MoMo,
+  Custom
 }
 
 enum KoishiAuthority {
@@ -34,21 +36,37 @@ interface LolimiApiResponse {
   }
 }
 
-export const Config: Schema<Config> = Schema.object({
-  chatRole: Schema.union([
-    Schema.const(ChatRole.JinFeng).description('婧枫（傲娇姐姐）'),
-    Schema.const(ChatRole.MoMo).description('沫沫（软萌妹妹）'),
-  ]).default(ChatRole.JinFeng).description('聊天角色'),
-  usePrefixTriggerInChannel: Schema.boolean().default(false).description('是否允许在频道（QQ群用户请理解成群聊）内以特定前缀触发聊天（也就是无需指令的方式），注意：私聊场景默认开启特定前缀触发聊天'),
-  prefix: Schema.string().default('chat').description('触发聊天所需要使用的前缀，聊天时前缀会被去除'),
-  authority: Schema.union([
-    Schema.const(KoishiAuthority.ALL_USER).description('全部用户 (1)'),
-    Schema.const(KoishiAuthority.PRO_USER).description('高级用户 (2)'),
-    Schema.const(KoishiAuthority.ADMIN).description('管理员 (3)'),
-    Schema.const(KoishiAuthority.PRO_ADMIN).description('高级管理员 (4)'),
-    Schema.const(KoishiAuthority.OWNER).description('所有者 (5)'),
-  ]).default(KoishiAuthority.OWNER).description('触发聊天/使用本插件指令所需要的最低权限等级，可参考[Koishi文档-深入定制机器人-用户权限](https://koishi.chat/zh-CN/manual/usage/customize.html#%E7%94%A8%E6%88%B7%E6%9D%83%E9%99%90)')
-});
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    chatRole: Schema.union([
+      Schema.const(ChatRole.JinFeng).description('婧枫（傲娇姐姐）'),
+      Schema.const(ChatRole.MoMo).description('沫沫（软萌妹妹）'),
+      Schema.const(ChatRole.Custom).description('自定义')
+    ]).default(ChatRole.JinFeng).description('聊天角色'),
+  }),
+  Schema.union([
+    Schema.object({
+      chatRole: Schema.const(ChatRole.Custom).required(),
+      chatRolePrompt: Schema.string().default('')
+        .role('textarea', { rows: [2, 5] })
+        .description('自定义角色所使用的角色设定提示词。')
+    }),
+    Schema.object({
+      chatRolePrompt: Schema.string().default('').hidden()
+    })
+  ]),
+  Schema.object({
+    usePrefixTriggerInChannel: Schema.boolean().default(false).description('是否允许在频道（QQ群用户请理解成群聊）内以特定前缀触发聊天（也就是无需指令的方式），注意：私聊场景默认开启特定前缀触发聊天'),
+    prefix: Schema.string().default('chat').description('触发聊天所需要使用的前缀，聊天时前缀会被去除'),
+    authority: Schema.union([
+      Schema.const(KoishiAuthority.ALL_USER).description('全部用户 (1)'),
+      Schema.const(KoishiAuthority.PRO_USER).description('高级用户 (2)'),
+      Schema.const(KoishiAuthority.ADMIN).description('管理员 (3)'),
+      Schema.const(KoishiAuthority.PRO_ADMIN).description('高级管理员 (4)'),
+      Schema.const(KoishiAuthority.OWNER).description('所有者 (5)'),
+    ]).default(KoishiAuthority.OWNER).description('触发聊天/使用本插件指令所需要的最低权限等级，可参考[Koishi文档-深入定制机器人-用户权限](https://koishi.chat/zh-CN/manual/usage/customize.html#%E7%94%A8%E6%88%B7%E6%9D%83%E9%99%90)')
+  })
+]);
 
 export function apply(ctx: Context, config: Config) {
   ctx.middleware(async (session, next) => {
@@ -60,7 +78,7 @@ export function apply(ctx: Context, config: Config) {
       // 为什么后面再判断权限？这是为了减少数据库查询次数，减缓压力
       const user = await ctx.database.getUser(session.platform, session.userId);
       if ((user.authority >= config.authority)) {
-        await session.send(`<quote id="${session.messageId}"/>${await requestReplyFromApi(ctx, config.chatRole, messageWithoutPrefix)}`);
+        await session.send(`<quote id="${session.messageId}"/>${await requestReplyFromApi(ctx, config, messageWithoutPrefix)}`);
       }
     }
 
@@ -72,23 +90,37 @@ export function apply(ctx: Context, config: Config) {
   }).example('lolimi-chat 你好').action(async (_, message) => {
     if (message === undefined || message === '') return '聊天内容不可为空';
 
-    return `<quote id="${_.session.messageId}"/>${await requestReplyFromApi(ctx, config.chatRole, message)}`;
+    return `<quote id="${_.session.messageId}"/>${await requestReplyFromApi(ctx, config, message)}`;
   });
 }
 
-async function requestReplyFromApi(ctx: Context, chatRole: ChatRole, message: string): Promise<string> {
+async function requestReplyFromApi(ctx: Context, config: Config, message: string): Promise<string> {
   let targetUrl: string;
-  switch (chatRole) {
+  switch (config.chatRole) {
     case ChatRole.JinFeng:
-      targetUrl = 'https://api.lolimi.cn/API/AI/jj.php';
+      targetUrl = `https://api.lolimi.cn/API/AI/jj.php?msg=${encodeURIComponent(message)}`;
       break;
     case ChatRole.MoMo:
-      targetUrl = 'https://api.lolimi.cn/API/AI/mm.php';
+      targetUrl = `https://api.lolimi.cn/API/AI/mm.php?msg=${encodeURIComponent(message)}`;
+      break;
+    case ChatRole.Custom:
+      targetUrl = `https://api.lolimi.cn/API/AI/mfcat3.5.php?sx=${encodeURIComponent(config.chatRolePrompt)}&msg=${encodeURIComponent(message)}`;
       break;
   }
-  const response: LolimiApiResponse = await ctx.http.get(`${targetUrl}?msg=${message}`);
-  if (response.code !== 200) {
-    return '请求API失败，请重试';
+  let response;
+  try {
+    response = await ctx.http.get(targetUrl);
+  } catch {
+    return '请求API失败，再来一次吧。';
   }
+
+  if (config.chatRole === ChatRole.Custom) {
+    return response;
+  }
+
+  if (response.code !== 200) {
+    return `请求API失败，再来一次吧。\ncode=${response.code}`;
+  }
+
   return response.data.output;
 }
